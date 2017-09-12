fc9cb62cd04a503945f41ec2de311b17d2332cb0
Russell Ivanovic <rustyshelf@gmail.com>
2017-09-12 11:58:03 +0930

    uncommented out deploy script

--- kotlin-example/deploy.sh
+++ kotlin-example/deploy.sh
@@ -4,7 +4,7 @@ set -x
 ./gradlew shadowJar
 
 #Deploy jar
-#scp build/libs/kotlin-example-fat.jar root@50.116.29.20:~/example/service_new.jar
+scp build/libs/kotlin-example-fat.jar root@50.116.29.20:~/example/service_new.jar
 
 #Stop the service, move the new jar file into place then restart it
 ssh root@50.116.29.20 '/root/example/finish_deployment.sh';
