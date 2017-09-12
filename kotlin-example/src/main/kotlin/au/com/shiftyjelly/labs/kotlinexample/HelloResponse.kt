80bb9a64969e5703edb3cf1c764175150b0a56ac
Russell Ivanovic <rustyshelf@gmail.com>
2017-09-12 12:04:08 +0930

    Basic JSON response example

---
+++ kotlin-example/src/main/kotlin/au/com/shiftyjelly/labs/kotlinexample/HelloResponse.kt
@@ -0,0 +1,6 @@
+package au.com.shiftyjelly.labs.kotlinexample
+
+class HelloResponse {
+    var message: String? = null
+    var code: Int = 0
+}
--- kotlin-example/src/main/kotlin/au/com/shiftyjelly/labs/kotlinexample/MainVerticle.kt
+++ kotlin-example/src/main/kotlin/au/com/shiftyjelly/labs/kotlinexample/MainVerticle.kt
@@ -2,22 +2,38 @@ package au.com.shiftyjelly.labs.kotlinexample
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.core.json.Json
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 
 class MainVerticle: AbstractVerticle() {
 
+    private val HTTP_OK = 200
+    private val PORT_NUMBER = 5000
+
     override fun start(startFuture: Future<Void>?) {
         val router = Router.router(vertx)
 
-        //setup endpoint
+        //setup endpoints
         router.get("/hello").handler({ this.handleHello(it) })
+        router.get("/jsonHello").handler({ this.handleHelloJson(it) })
 
         //start our server
-        vertx.createHttpServer().requestHandler( { router.accept(it) }).listen(5000)
+        vertx.createHttpServer().requestHandler( { router.accept(it) }).listen(PORT_NUMBER)
     }
 
     private fun handleHello(routingContext: RoutingContext) {
-        routingContext.response().setStatusCode(200).end("Well hello yourself!")
+        routingContext.response().setStatusCode(HTTP_OK).end("Well hello yourself!")
+    }
+
+    private fun handleHelloJson(routingContext: RoutingContext) {
+        val response = HelloResponse()
+        response.message = "Welcome stranger"
+        response.code = 150
+
+        val jsonObject = JsonObject(Json.encode(response))
+
+        routingContext.response().setChunked(true).write(jsonObject.encodePrettily()).putHeader("Content-Type", "application/json").setStatusCode(HTTP_OK).end()
     }
 }
